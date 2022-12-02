5ded3528476a640f710c9e960ef611a9552ebd09
Nícolas Félix <nf.oliveira@hotmail.com>
2022-12-02 00:03:04 -0300

    Testando RestController

---
+++ src/main/java/com/felixweb/projeto/resources/CategoriaResource.java
@@ -0,0 +1,33 @@
+package com.felixweb.projeto.resources;
+
+import com.felixweb.projeto.domain.Categoria;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Categoria> listar () {
+
+        Categoria cat1 = new Categoria(1,"Informatica");
+        Categoria cat2 = new Categoria(2,"Eletronicos");
+        Categoria cat3 = new Categoria(2,"Eletrodomesticos");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+        lista.add(cat3);
+
+        return lista;
+    }
+
+
+
+
+}
